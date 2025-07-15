a7acaa9eabbb391084c016fd2bebdbb223982842
Denise Bagnell <dbagnell@gmail.com>
2025-07-15 12:33:10 -0400

    Updated play.js to include setupInput and handleUserInput functions

--- play.js
+++ play.js
@@ -5,16 +5,27 @@ const connection = connect();
 
 // function to set up input from stdin
 
-const setupInput = function(conn) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
- stdin.on('data', (key) => {
+const handleUserInput = function(key) {
   if (key === '\u0003') {
-    process.exit(); // Ctrl+C exits
+    console.log('Exiting game...');
+    process.exit();
   }
+};
+
+const setupInput = function(conn) {
+  const stdin = process.stdin; // create variable to hold the stdin object so we don't have to type process.stdin multiple times
+  stdin.setRawMode(true); // raw mode
+  stdin.setEncoding('utf8'); // utf8 encoding
+  stdin.resume(); // resume stdin so program can listen for input
+  return stdin; // return stdin object so we can use it elsewhere in program
+
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit(); // Ctrl+C exits
+    }
+  });
+
+
 
   const moves = {
     w: "Move: up",
@@ -26,10 +37,6 @@ const setupInput = function(conn) {
   if (moves[key]) {
     conn.write(moves[key]);
   }
-});
-
-
-  return stdin;
 };
 
 setupInput(connection);
